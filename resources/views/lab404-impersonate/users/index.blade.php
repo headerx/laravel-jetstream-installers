e7cb3147f390c6eba4088778bda60a00ee2430f3
inmanturbo <inman.turbo@gmail.com>
2022-02-26 13:59:56 -0500

    work in progress

--- resources/views/lab404-impersonate/users/index.blade.php
+++ resources/views/lab404-impersonate/users/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('lumki::ui.manage_users') }}
+            {{ __('Impersonate Users') }}
         </h2>
     </x-slot>
 
